666ee5089d9a1b57372b924fed4e3ebe128ee0ed
ndubouil <ndubouil@student.42.fr>
2017-12-09 12:04:20 +0100

    ADD norm all the files, just the comments | OK coucou Nelson

--- ft_filetolist.c
+++ ft_filetolist.c
@@ -6,7 +6,7 @@
 /*   By: ndubouil <ndubouil@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/12/08 09:22:14 by ndubouil          #+#    #+#             */
-/*   Updated: 2017/12/09 11:09:09 by ndubouil         ###   ########.fr       */
+/*   Updated: 2017/12/09 11:58:59 by ndubouil         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,35 +26,35 @@ static void		ft_tempisnl(char temp)
 }
 
 /*
- ** Params : file of tetriminos
- ** Return : t_list of a struct of tetriminos
- ** Convert the file in a t_list of struct of tetriminos
- ** Open -> Read while the file is not finish and put in t_list for all of tetriminos -> Close
- */
+** Params : file of tetriminos
+** Return : t_list of a struct of tetriminos
+** Convert the file in a t_list of struct of tetriminos
+** Open -> Read while the file is not finish and put in t_list for all of tetriminos -> Close
+*/
 t_list			*ft_filetolist(char *file)
 {
 	int				i;
 	int				fd;
 	int				n_lus;
-	char			temp[BUFF_SIZE];
+	char			tp[BUFF_SIZE];
 	t_list			*lst;
 
 	lst = NULL;
 	i = 0;
 	if ((fd = open(file, O_RDONLY)) < 0)
 		ft_print_error();
-	while ((n_lus = read(fd, temp, BUFF_SIZE)) > 0)
+	while ((n_lus = read(fd, tp, BUFF_SIZE)) > 0)
 	{
-		ft_tempisnl(temp[0]);
-		temp[n_lus] = '\0';
+		ft_tempisnl(tp[0]);
+		tp[n_lus] = '\0';
 		if (i == 0)
-			lst = ft_lstnew(ft_newtetri(i, temp), sizeof(t_tetri));
+			lst = ft_lstnew(ft_newtetri(i, tp), sizeof(t_tetri));
 		else
-			ft_lstaddend(&lst, ft_lstnew(ft_newtetri(i, temp), sizeof(t_tetri)));
+			ft_lstaddend(&lst, ft_lstnew(ft_newtetri(i, tp), sizeof(t_tetri)));
 		i++;
-		read(fd, temp, 1);
+		read(fd, tp, 1);
 	}
-	ft_tempisnl(temp[0]);
+	ft_tempisnl(tp[0]);
 	ft_isempty_or_toolong(i, n_lus);
 	close(fd);
 	return (lst);
